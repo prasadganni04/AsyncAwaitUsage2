b60077303411cd4546f2d286a8a2d9a37b5d2172
prasadganni04 <prasadganni04@gmail.com>
2021-04-05 13:40:13 +0530

    AsyncAwaitUsage2

---
+++ AsyncAwaitUsage2/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace AsyncAwaitUsage2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblcount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblcount
+            // 
+            this.lblcount.AutoSize = true;
+            this.lblcount.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcount.Location = new System.Drawing.Point(339, 90);
+            this.lblcount.Name = "lblcount";
+            this.lblcount.Size = new System.Drawing.Size(140, 29);
+            this.lblcount.TabIndex = 0;
+            this.lblcount.Text = "Press Here!";
+            this.lblcount.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblcount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblcount;
+    }
+}
+
---
+++ AsyncAwaitUsage2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace AsyncAwaitUsage2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private  async void label1_Click(object sender, EventArgs e)
+        {
+            Task<int> m = new Task<int>(CountChar);
+            m.Start();
+
+
+
+            lblcount.Text = "Started.....Please wait";
+            int c = await m;
+            lblcount.Text = c.ToString() + " Characters in file";
+
+        }
+        private int CountChar()
+        {
+            int c = 0;
+            using (StreamReader sr = new StreamReader("C:\\Users\\GVSS Prasad\\OneDrive\\Desktop\\testtack access.txt")) 
+            {
+                string str = sr.ReadToEnd();
+                c = str.Length;
+                Thread.Sleep(5000);
+
+             };
+            return c;
+           
+        }
+    }
+}
